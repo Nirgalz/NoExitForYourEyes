58da0939e9d459e1203a2d135f2f08f7800667d3
Nirgalz <znirgal@gmail.com>
2018-10-08 17:29:54 +0200

    string work in progress

--- script.js
+++ script.js
@@ -3,50 +3,52 @@
 $(document).ready(() => {
 
     var options = {
-        devMode: false
+        devMode: true
     }
-
 
     var userData = {
         name: "",
-        status: ""
+        status: "status test"
     };
 
 
-    var data = {
-        welcome: {
-            content: " Hello human. For the sake of subject identification, can you please decline your identity ?",
+    var data = [{
+        label: "welcome",
+            string: " Hello human. For the sake of subject identification, can you please decline your identity ?",
             nextAction: () => textInputHideShow(true)
         },
-        greeting: {
-            content: " hi " + userData.name + ", I\'m your personal assistant for your time being, which may end up pretty soon. You've been removed from any control of your body. Don't despair yet, I can help you with that.",
+        {
+            label: "greeting",
+            string: " hi {name}, I\'m your personal assistant for your time being, which may end up pretty soon. You've been removed from any control of your body. Don't despair yet, I can help you with that. {status}",
             nextAction: () => {
                 console.log(userData.name)
             }
 
         }
-    };
+    ];
+
     textInputHideShow(false);
 
-    textOutput(data.welcome, 0);
+    textOutput(data[0], 0);
 
     //output text and next action on end
     function textOutput(ctx, count) {
-
+        replaceVarsInText(ctx.string);
         const div = $("#textOutput");
         let out = $("#out");
         if (count === 0) {
             out.first().removeAttr("id");
             div.append("<p id='out'></p>");
         }
+
         if (options.devMode) {
-            out.text(ctx.content);
+            out.text(ctx.string);
             ctx.nextAction();
         } else {
-            if (ctx.content[count] !== undefined) {
+            if (ctx.string[count] !== undefined) {
 
                 setTimeout(() => {
-                    out.text(out.text() + ctx.content[count]);
+                    out.text(out.text() + ctx.string[count]);
                     textOutput(ctx, count + 1)
                 }, 30);
 
@@ -55,6 +57,26 @@ $(document).ready(() => {
             }
         }
     }
+
+
+    function replaceVarsInText(string) {
+        let s = string.split("{");
+        let vars = [];
+        if (s.length > 1) {
+            for (let i = 1 ; i < s.length ; i++) {
+                vars.push(s[i].split("}")[0]);
+            }
+        }
+        for (let j = 0 ; j < vars.length ; j++) {
+            let v = "{" + vars[j] + "}";
+            let c = userData[vars[j]];
+            string.replace(v, c);
+            console.log(string, v, c);
+        }
+
+        console.log(string);
+    }
+
 
     function textInputHideShow(bool) {
         const input = $("#textInput");
@@ -75,8 +97,7 @@ $(document).ready(() => {
 
             userData.name = input.val();
             textInputHideShow(false);
-            console.log(userData.name)
-            textOutput(data.greeting, 0);
+            textOutput(data[1], 0);
 
         }
     });
