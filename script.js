9d531fd6143f26c8e8dfa37f5072adf000395edd
Nirgalz <znirgal@gmail.com>
2018-10-09 16:31:05 +0200

    added tts

--- script.js
+++ script.js
@@ -8,28 +8,48 @@ $(document).ready(() => {
 
     var userData = {
         name: "",
-        status: "status test"
+        status: "welcome"
     };
 
 
-    var data = [{
-        label: "welcome",
+    var data = {
+        welcome : {
             string: " Hello human. For the sake of subject identification, can you please decline your identity ?",
-            nextAction: () => textInputHideShow(true)
-        },
-        {
-            label: "greeting",
-            string: " hi {name}, I\'m your personal assistant for your time being, which may end up pretty soon. You've been removed from any control of your body. Don't despair yet, I can help you with that.",
             nextAction: () => {
-                console.log(userData.name)
+                userData.status = "greeting";
+                textInputHideShow(true);
             }
-
+        },
+        greeting : {
+            string: " Salutations {name}, I\'m your personal assistant for your time being, which may end up pretty soon. You've been removed from any control of your body. Don't despair yet, I can help you with that.",
+            nextAction: () => {
+                userData.status = "proposeInformation";
+                contextAction();
+            }
+        },
+        proposeInformation: {
+            string: " You may need more data to process the situation, but we have other matters in hand. Your physical integrity is in danger. Do you wish to be informed of the immediate risk or better assess the situation ?",
+            nextAction: () => {
+                userData.status = "firstChoice";
+            }
+        },
+        firstChoice : {
+            type: "choice",
+            string : [
+                ""
+            ]
         }
-    ];
+    };
 
     textInputHideShow(false);
 
-    textOutput(data[0], 0);
+    //starts game
+    contextAction();
+
+    function contextAction() {
+        textOutput(data[userData.status], 0);
+        PlaySynthTts(data[userData.status].string);
+    }
 
     //output text and next action on end
     function textOutput(ctx, count) {
@@ -73,13 +93,13 @@ $(document).ready(() => {
         return string;
     }
 
-
+    //hide-show text input
     function textInputHideShow(bool) {
         const input = $("#textInput");
         if (bool) {
-            input.show()
+            input.show();
         } else {
-            input.hide()
+            input.hide();
         }
     }
 
@@ -88,9 +108,46 @@ $(document).ready(() => {
     $("#textInput").keypress((e) => {
         if (e.which === 13) {
             let input = $("#textInput");
-            userData.name = input.val();
             textInputHideShow(false);
-            textOutput(data[1], 0);
+            eventManager(input.val());
         }
     });
+
+    function eventManager(input) {
+        if (userData.status === "greeting") {
+            userData.name = input;
+            contextAction();
+        }
+
+    }
+
+
+
+
+
+//tests with TTS
+function PlaySynthTts(string) {
+    let seconds = 30;
+    let waveBytes = SAMPLE_FREQUENCY * 2 * 2 * seconds;
+    let speed = 0.3;
+    let buf = new Int16Array(new ArrayBuffer(waveBytes));
+    let freq = 40;
+    SynthSpeech(buf, string, freq, speed, 0);
+    playAudioBuffer(buf); 
+}
+
+function playAudioBuffer(buf) {
+    var maxAmp = 22000;
+    var audioString = "";
+    for (var i=0; i<buf.length; i++) {
+        var y = buf[i] / maxAmp * 0x7800;
+        audioString += String.fromCharCode(y & 255, (y >> 8) & 255);
+    }
+    console.log("new Audio...");
+    var audio = new Audio("data:audio/wav;base64,"+btoa(atob("UklGRti/UABXQVZFZm10IBAAAAABAAIARKwAABCxAgAEABAAZGF0YbS/UAA") + audioString));
+    console.log("play()!");
+    audio.play();
+}
+
+
 });
